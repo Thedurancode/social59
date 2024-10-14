636c9e534d17282aa0cbedf0c8a68aaa2cef06f3
Ed Duran <123636289+Thedurancode@users.noreply.github.com>
2024-10-14 10:10:06 -0400

    update

---
+++ contact.js
@@ -0,0 +1,10 @@
+const restaurantInfo = {
+  name: "59 Eatery & Lounge",
+  address: "512 59th St, West New York, NJ 07093",
+  phone: "(201) 751-5999",
+  social: {
+    facebook: "https://www.facebook.com/59EateryAndLounge",
+    instagram: "https://www.instagram.com/59eateryandlounge",
+    // Add more social media properties as needed
+  }
+};
